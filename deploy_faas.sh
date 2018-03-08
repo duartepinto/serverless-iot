561adfdf943094c7cb02b9de951215467b2d3505
444Duarte <duartemr.pinto@gmail.com>
2018-03-08 11:38:38 +0000

    removing unnecessary functions

--- deploy_faas.sh
+++ deploy_faas.sh
@@ -2,4 +2,15 @@
 cd faas
 sudo sh deploy_stack.sh 
 cd ..
+
+#REMOVE UNWANTED FUNCTIONS
+faas-cli remove func_echoit
+faas-cli remove func_wordcount
+faas-cli remove func_base64
+faas-cli remove func_markdown 
+faas-cli remove func_hubstats
+
+
+#CLEAN DOCKER
+sudo docker system prune
 # exit
