2318538859b6f48c17e083cdaf2566a2602009db
444Duarte <duartemr.pinto@gmail.com>
2018-03-21 18:11:04 +0000

    changed template for node.js

--- template/node/function/handler.js
+++ template/node/function/handler.js
@@ -4,10 +4,25 @@
 /*jshint asi: true */
 'use strict';
 
-const express = require('express')
-const app = express()
-
 function handle(req) {
+    var data = {}
+    if(req == undefined || req == null){
+        data.status= "error1"
+        data.message = "Request (req) undefined or null"
+        console.info(JSON.stringify(data))
+        return
+    }
+
+    try{
+        req = JSON.parse(req)
+
+    }catch (err) {
+        data.status= "error"
+        data.message = "" + err
+        console.info(JSON.stringify(data))
+        return 
+    }   
+
     console.log('Hello! You said:', req)
 }
 
