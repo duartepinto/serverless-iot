b6df691f5da8def959a8efb9e1f7cd88f4bc13c0
444Duarte <duartemr.pinto@gmail.com>
2018-04-25 18:06:13 +0100

    retrieving rewards

--- proxy/weight_scale/handler.js
+++ proxy/weight_scale/handler.js
@@ -12,7 +12,9 @@ const queryList = {
     average_duration_seconds :{
         query : "gateway_functions_seconds_sum/gateway_functions_seconds_count"
     },
-    mab_average_duration_seconds : {
+    mab_duration_seconds : {
+        querySecondsSum : "gateway_functions_seconds_sum",
+        queryCount : "gateway_functions_seconds_count",
     }
 }
 
@@ -61,7 +63,25 @@ function handle(req) {
     var cloudPromise = getCloudWeight(reqFunc, query)
     
     Promise.all([localPromise, cloudPromise])
-        .then(returnWeights)
+        .then((results) =>{ 
+            switch(query){
+                case queryList.mab_duration_seconds:
+                    data.status = "success"
+                    data.result = results
+
+                    return console.info(JSON.stringify(data))
+                    break
+
+                case queryList.average_duration_seconds:
+                    returnWeights(results)
+                    break
+                default:
+                    data.status = "error"
+                    data.result = results
+                    return console.info(JSON.stringify(data))
+
+            }
+        })
         .catch( (err) => {
             data.status= "error"
             data.message = "" + err
@@ -78,19 +98,57 @@ function returnWeights(results) {
 }
 
 function getLocalWeight(functionName, query){
-    
+    var host = rigConfigs.localUrl + ":" +  rigConfigs.prometheusPort
+    var path = "/api/v1/query"
 
     return new Promise(function(resolve, reject){
 
         switch(query){
 
-            case queryList.mab_average_duration_weight:
-                reject("Not implmented")
+            case queryList.mab_duration_seconds:
+                var parameters1 = "?query=" + query.queryCount + '{function_name="'+functionName + '"}'
+                var parameters2 = "?query=" + query.querySecondsSum + '{function_name="'+functionName + '"}'
+                var urlCount = host + path + parameters1
+                var urlSecondsSum = host + path + parameters2
+
+                var promiseCount = new Promise( (countResolve, countReject) => {
+                    request.get(urlCount, function(err, resp, body) {
+                        if (err) {
+                            return reject(err);
+                        } else {
+                            body = JSON.parse(body)
+                            if(body.status === "success" && body.data.result[0] != undefined){
+                                return countResolve(body.data.result[0].value[1])
+                            }
+                            countResolve(null)
+                        }
+                    })
+
+                })
+
+                var promiseSum = new Promise( (sumResolve, sumReject) => {
+                    request.get(urlSecondsSum, function(err, resp, body) {
+                        if (err) {
+                            return sumReject(err);
+                        } else {
+                            body = JSON.parse(body)
+                            if(body.status === "success" && body.data.result[0] != undefined){
+                                return sumResolve(body.data.result[0].value[1])
+                            }
+                            sumResolve(null)
+                        }
+                    })
+                })
+                Promise.all([promiseCount, promiseSum])
+                    .then(results =>{ 
+                        resolve(results)
+                    }).catch((err) => {
+                        reject(err)
+                    })
+
                 break
 
             case queryList.average_duration_weight:
-                var host = rigConfigs.localUrl + ":" +  rigConfigs.prometheusPort
-                var path = "/api/v1/query"
                 var parameters = "?query=" + query.query + '{function_name="'+functionName + '"}'
 
                 var url = host + path + parameters
@@ -124,8 +182,20 @@ function getCloudWeight(functionName, query){
 
     return new Promise(function(resolve, reject){
         switch(query){
-            case queryList.mab_average_duration_seconds: 
-                reject("Not implemented")
+            case queryList.mab_duration_seconds: 
+                request.post({url, json: body}, function(err, resp, body) {
+                    if (err) {
+                        reject(err);
+                        resolve(null)
+                    } else {
+                        if(body.status === "success" && body.items.length > 0){
+                            return resolve(body.items)
+                        }
+                        
+                        return resolve(null)
+                    }
+                })
+
                 break
             case queryList.average_duration_seconds:
 
